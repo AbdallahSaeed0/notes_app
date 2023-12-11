61910c8b7db153debd2510d71928939572f36fb2
AbdallahSaeed0 <bodyahly17@gmail.com>
2023-12-11 20:25:17 +0200

    creatinghome page

---
+++ lib/views/notes_view.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class NotesView extends StatelessWidget {
+  NotesView({super.key});
+
+  String id = "notesview";
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
