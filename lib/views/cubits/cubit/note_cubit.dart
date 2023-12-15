43f190ed24611464e8d25e6e177dd5dafcdcd227
AbdallahSaeed0 <bodyahly17@gmail.com>
2023-12-15 03:45:47 +0200

    create fetch method

--- lib/views/cubits/cubit/note_cubit.dart
+++ lib/views/cubits/cubit/note_cubit.dart
@@ -1,9 +1,26 @@
 import 'package:bloc/bloc.dart';
+import 'package:hive/hive.dart';
 import 'package:meta/meta.dart';
+import 'package:notes_app/views/constants.dart';
 import 'package:notes_app/views/models/note_model.dart';
 
 part 'note_state.dart';
 
 class NoteCubit extends Cubit<NoteState> {
   NoteCubit() : super(NoteInitial());
+
+  fetchAllNotes() {
+    addNote(NoteModel note) async {
+      emit(NoteLoading());
+
+      try {
+        var notesbox = Hive.box<NoteModel>(kNotesbox);
+
+        List<NoteModel> notes = notesbox.values.toList();
+        emit(NoteSuccess(notes));
+      } catch (e) {
+        emit(NoteFaliure(e.toString()));
+      }
+    }
+  }
 }
--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'cubits/cubit/note_cubit.dart';
 import 'widgets/modal_bottom_sheet.dart';
 import 'widgets/note_list_view.dart';
 import 'widgets/notes_appbar.dart';
@@ -10,29 +12,32 @@ class NotesView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          ModalBottomSheet(context);
-        },
-        child: const Icon(Icons.add),
-      ),
-      body: const Padding(
-        padding: EdgeInsets.symmetric(horizontal: 24),
-        child: Column(
-          children: [
-            SizedBox(
-              height: 50,
-            ),
-            NotesAppbar(
-              title: "Note",
-              icon: Icons.search,
-            ),
-            SizedBox(
-              height: 40,
-            ),
-            NoteListView(),
-          ],
+    return BlocProvider(
+      create: (context) => NoteCubit(),
+      child: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            ModalBottomSheet(context);
+          },
+          child: const Icon(Icons.add),
+        ),
+        body: const Padding(
+          padding: EdgeInsets.symmetric(horizontal: 24),
+          child: Column(
+            children: [
+              SizedBox(
+                height: 50,
+              ),
+              NotesAppbar(
+                title: "Note",
+                icon: Icons.search,
+              ),
+              SizedBox(
+                height: 40,
+              ),
+              NoteListView(),
+            ],
+          ),
         ),
       ),
     );
